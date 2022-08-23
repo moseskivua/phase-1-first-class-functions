d6894cb041fdfe6c69bcf29866275a10bafa4d41
Moses Kivua <kivuamoses9@gmail.com>
2022-08-23 11:27:22 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,15 @@
+const returnFirstTwoDrivers = function (drivers){
+    return drivers.slice(0,2);
+    }
+const returnLastTwoDrivers = function (drivers){
+    return drivers.slice(-2);
+}
+const selectingDrivers=[returnFirstTwoDrivers,returnLastTwoDrivers];
+const createFareMultiplier =function(fareMultiplier){
+    return function(value){return fareMultiplier*value};
+}
+const fareDoubler = createFareMultiplier(2)
+const fareTripler =createFareMultiplier(3);
+const selectDifferentDrivers = function (drivers,func){
+    return func(drivers);
+}
